be33634b22d7e9ca644be780b62bb6b577edcc48
Phalguni  S <114557138+phalgunisp@users.noreply.github.com>
2022-10-20 07:40:47 +0000

    problem solved

---
+++ set01/problem01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("phalguni s p \n");
+  return 0;
+}
---
+++ set01/problem02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+  int a= 6;
+  int b= 4;
+  int sum;
+  sum = a+b;
+  printf("%d" , sum);
+  
+}
